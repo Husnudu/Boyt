c7ced184861bad98f052c235cad6d5506a790b90
DucTheVulpe <41674548+DucTheVulpe@users.noreply.github.com>
2022-06-08 18:53:07 +0400

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/goqerti/Userator -b master
+        git clone https://github.com/DucTheVulpe/UseratorBot -b master
         save_prog "bot_clone"
     fi
     echo "DONE!!"
